f9276f474b3afa95fde31210128d14785c45c7ea
Jeremy Thomerson <jeremy@thomersonfamily.com>
2024-04-04 00:08:16 -0400

    feat: first pass at media player UI MVP

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header :translucent="true" v-if="!(selectedBook && selectedChapter)">
       <ion-toolbar>
         <ion-title>Bible Watcher</ion-title>
       </ion-toolbar>
@@ -35,18 +35,14 @@
             <button @click="selectedBook = undefined">&lt;</button>
             {{ selectedBook.name }}
           </h3>
-          <div class="books">
-            <div class="book" v-for="chap in availableChapters" :key="chap" @click="selectedChapter = chap">
+          <div class="chapters">
+            <div class="chapter" v-for="chap in availableChapters" :key="chap" @click="selectedChapter = chap">
               {{ chap }}
             </div>
           </div>
         </div>
         <div class="mediaPlayer" v-if="selectedBook && selectedChapter">
-          <h3>
-            <button @click="selectedChapter = undefined">&lt;</button>
-            {{ selectedBook.name }} {{ selectedChapter }}
-          </h3>
-          <div id="videoPlayer" v-if="playerData">
+          <div class="videoPlayer" v-if="playerData">
             <video ref="videoEl"
               playsinline muted controls="false"
               poster="https://placebear.com/1920/1080"
@@ -57,37 +53,61 @@
               v-bind:src="playerData.spoken.url"
             ></audio>
           </div>
-          <div id="playerControls" v-if="playerData">
-            <button @click="playPause()">
-              <span v-if="playbackStatus.playing">Pause</span>
-              <span v-if="!playbackStatus.playing">Play</span>
-            </button>
-          </div>
-          <div id="playerDebug" v-if="playerData">
-            <br>
-            Video rate:
-            <button @click="updateVideoPlaybackRate(-0.2)">-</button>
-            {{ playbackStatus.videoPlaybackRate.toFixed(1) }}
-            <button @click="updateVideoPlaybackRate(0.2)">+</button>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            Audio rate: {{ playbackStatus.audioPlaybackRate.toFixed(3) }}
-            <br>
-            Video: {{ playbackStatus.videoTime.toFixed(3) }}
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            Audio: {{ playbackStatus.audioTime.toFixed(3) }}
-            <br>
-            Current Marker:
-            {{ playbackStatus.currentMarker?.label || "None" }}
-          </div>
-          <div class="markerListContainer" v-if="playerData?.markers.length">
-            <ul class="markers">
-              <li v-for="mrkr in playerData.markers" :key="mrkr.id"
-                @click="jumpToMarker(mrkr)"
-                :class="playbackStatus.currentMarker?.id === mrkr.id ? 'active' : ''"
-                >
-                {{ mrkr.label }}
-            </li>
-            </ul>
+          <div class="mediaPlayerOverlay" ref="mediaPlayerOverlay"
+            @click="toggleOverlay()">
+            <div class="overlayHeader">
+              <h3>
+                <button @click.stop="selectedChapter = undefined">&lt;</button>
+                {{ selectedBook.name }} {{ selectedChapter }}
+              </h3>
+            </div>
+            <div class="playerControls" v-if="playerData">
+              <button @click.stop="changeMarkerByIncrement(-1)">
+                Prev
+              </button>
+              <button @click.stop="seekVideo(-5)">
+                -5s
+              </button>
+              <button @click.stop="playPause()">
+                <span v-if="playbackStatus.playing">Pause</span>
+                <span v-if="!playbackStatus.playing">Play</span>
+              </button>
+              <button @click.stop="seekVideo(+15)">
+                +15s
+              </button>
+              <button @click.stop="changeMarkerByIncrement(+1)">
+                Next
+              </button>
+            </div>
+            <div class="playerDebug" v-if="playerData">
+              <div class="debugInfo">
+                Video rate:
+                <button @click.stop="updateVideoPlaybackRate(-0.2)">-</button>
+                {{ playbackStatus.videoPlaybackRate.toFixed(1) }}
+                <button @click.stop="updateVideoPlaybackRate(0.2)">+</button>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                Audio rate: {{ playbackStatus.audioPlaybackRate.toFixed(3) }}
+                <br>
+                Video: {{ playbackStatus.videoTime.toFixed(3) }}
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                Audio: {{ playbackStatus.audioTime.toFixed(3) }}
+                <br>
+                Current Marker:
+                {{ playbackStatus.currentMarker?.label || "None" }}
+              </div>
+            </div>
+            <div class="markerListContainer"
+              @click="toggleOverlay()"
+              v-if="playerData?.markers.length">
+              <ul class="markers">
+                <li v-for="mrkr in playerData.markers" :key="mrkr.id"
+                  @click.stop="jumpToMarker(mrkr)"
+                  :class="playbackStatus.currentMarker?.id === mrkr.id ? 'active' : ''"
+                  >
+                  {{ mrkr.label }}
+              </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div id="loading" v-if="loading">Loading...</div>
@@ -133,7 +153,10 @@ const data = new WebsiteData(),
       playerData: Ref<MediaData | undefined> = ref(),
       playbackStatus: Ref<MediaPlaybackStatus> = ref(emptyPlaybackStatus()),
       videoEl = ref<HTMLVideoElement>(),
-      audioEl = ref<HTMLAudioElement>();
+      audioEl = ref<HTMLAudioElement>(),
+      mediaPlayerOverlay = ref<HTMLDivElement>();
+
+let audioPlaybackRateAdjustmentsPaused = false;
 
 function playPause() {
   if (playbackStatus.value.playing) {
@@ -141,6 +164,44 @@ function playPause() {
   } else {
     audioEl.value?.play();
   }
+}
+
+function changeMarkerByIncrement(inc: number) {
+  if (!playerData.value || !playbackStatus.value || !playbackStatus.value.currentMarker) {
+    return;
+  }
+
+  const currentMarkerInd = playerData.value.markers.indexOf(playbackStatus.value.currentMarker),
+        desiredMarkerInd = currentMarkerInd >= 0 ? (currentMarkerInd + inc) : -1;
+
+  if (desiredMarkerInd < 0 || desiredMarkerInd >= playerData.value.markers.length) {
+    return;
+  }
+
+  return jumpToMarker(playerData.value.markers[desiredMarkerInd]);
+}
+
+function seekVideo(v: number) {
+  if (!videoEl.value) {
+    return;
+  }
+
+  const desiredTime = videoEl.value.currentTime + v;
+
+  videoEl.value.currentTime = Math.max(0, Math.min(desiredTime, videoEl.value.duration));
+  // Updating the audio playback rate also seeks it to the proper location if it's not
+  // close (within one second), so we take advantage of that here.
+  // TODO: probably need to make this adjustment happen at the next time update from the
+  // audio player so that we know it actually finished its seek:
+  updateAudioPlaybackRate();
+}
+
+function toggleOverlay() {
+  if (!mediaPlayerOverlay.value) {
+    throw new Error('no overlay found');
+  }
+
+  mediaPlayerOverlay.value.classList.toggle('hidden');
 }
 
 function updateVideoPlaybackRate(inc: number) {
@@ -154,15 +215,25 @@ function updateVideoPlaybackRate(inc: number) {
 }
 
 function jumpToMarker(mrkr: MatchedMarker): void {
-  if (!videoEl.value) {
+  if (!videoEl.value || !audioEl.value) {
     return;
   }
 
+  audioPlaybackRateAdjustmentsPaused = true;
   playbackStatus.value.currentMarker = mrkr;
   videoEl.value.currentTime = mrkr.signed.start;
+  audioEl.value.currentTime = mrkr.spoken.start;
+  audioPlaybackRateAdjustmentsPaused = false;
+  // TODO: probably need to make this adjustment happen at the next time update from the
+  // audio player so that we know it actually finished its seek:
+  updateAudioPlaybackRate();
 }
 
 function updateAudioPlaybackRate(): void {
+  if (audioPlaybackRateAdjustmentsPaused) {
+    return;
+  }
+
   const $vid = videoEl.value,
     $aud = audioEl.value,
     marker = playbackStatus.value.currentMarker;
@@ -182,11 +253,11 @@ function updateAudioPlaybackRate(): void {
     // of where this audio marker should start
     isAudioClose = ($aud.currentTime >= (audExpectedTime - 1)) && ($aud.currentTime <= (audExpectedTime + 1));
 
-  console.info(`vid at ${$vid.currentTime.toFixed(2)} of ${marker.signed.start.toFixed(2)}-${marker.signed.end.toFixed(2)} (${(vidPastPctg * 100).toFixed(2)}% of ${vidDuration.toFixed(2)} sec); aud at ${$aud.currentTime.toFixed(2)} (expected ${audExpectedTime.toFixed(2)}) of ${marker.spoken.start.toFixed(2)}-${marker.spoken.end.toFixed(2)} (${(audPastPctg * 100).toFixed(2)}% of ${audDuration.toFixed(2)} sec)`);
+  console.debug(`vid at ${$vid.currentTime.toFixed(2)} of ${marker.signed.start.toFixed(2)}-${marker.signed.end.toFixed(2)} (${(vidPastPctg * 100).toFixed(2)}% of ${vidDuration.toFixed(2)} sec); aud at ${$aud.currentTime.toFixed(2)} (expected ${audExpectedTime.toFixed(2)}) of ${marker.spoken.start.toFixed(2)}-${marker.spoken.end.toFixed(2)} (${(audPastPctg * 100).toFixed(2)}% of ${audDuration.toFixed(2)} sec)`);
 
   if (!isAudioClose) {
     console.info(`audio was not close: at ${$aud.currentTime}, expected ${audExpectedTime}, so seeking`);
-    $aud.currentTime = marker.spoken.start;
+    $aud.currentTime = audExpectedTime;
   }
 
   const vidRemaining = marker.signed.end - $vid.currentTime,
@@ -239,11 +310,10 @@ watch([ videoEl, audioEl ], () => {
     }
 
     const marker = playerData.value.markers.find((m) => {
-      return $vid.currentTime >= m.signed.start && $vid.currentTime <= m.signed.end;
+      return $vid.currentTime >= m.signed.start && $vid.currentTime < m.signed.end;
     });
 
     if (marker !== playbackStatus.value.currentMarker) {
-      console.info('signed marker change', marker, playbackStatus);
 
       if (!marker) {
         return;
@@ -323,47 +393,23 @@ watch([ selectedBook, selectedChapter ], async () => {
   transform: translateY(-50%);
 }
 
-.contentArea {
+.bookCollection, .chapterCollection {
   margin: 1em;
 }
 
-video {
-  max-width: 100%;
-}
-
-audio {
-  width: 100%;
-  display: none;
-}
-
-.markerListContainer .markers {
-  padding: 0;
-  margin: 0;
-  margin-top: 1em;
-  padding-left: 1em;
-}
-
-.markerListContainer .markers li {
-  list-style-type: none;
-  padding: 1em;
-  border: 1px solid rgba(255, 255, 255, 0.6);
-}
-
-.markerListContainer .markers li.active {
-  background-color: rgba(255, 255, 255, 0.15);
-}
-
-.books {
+.books, .chapters {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   column-gap: 0.3em;
   row-gap: 0.3em;
 }
-
-.books .book {
+.books .book, .chapters .chapter {
   border: 1px solid grey;
   padding: 0.8em;
   text-align: center;
+}
+.books .book {
+  color: rgb(240, 240, 240);
 }
 .book.bk1, .book.bk2, .book.bk3, .book.bk4, .book.bk5,
 .book.bk23, .book.bk24, .book.bk25, .book.bk26, .book.bk27, .book.bk28, .book.bk29,
@@ -383,5 +429,154 @@ audio {
 .book.bk53, .book.bk54, .book.bk55, .book.bk56, .book.bk57, .book.bk58, .book.bk59,
 .book.bk60, .book.bk61, .book.bk62, .book.bk63, .book.bk64, .book.bk65 {
   background-color: rgb(39, 81, 151);
+}
+
+.contentArea {
+  margin: 0;
+}
+
+.mediaPlayer { /** the "page" for media playback */
+  position: relative;
+  /* display: inline-block; */
+  width: 100vw;
+  height: 100vh;
+}
+
+.videoPlayer {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+video {
+  width: 100%;
+  max-width: 100vw;
+  max-height: 100vh;
+}
+
+audio {
+  width: 100%;
+  display: none;
+}
+
+.mediaPlayerOverlay {
+  background-color: rgba(0, 0, 0, 0.4);
+  color: rgb(240, 240, 240);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+
+  padding-top: var(--ion-safe-area-top);
+  padding-left: var(--ion-safe-area-left);
+  padding-right: var(--ion-safe-area-right);
+  padding-bottom: var(--ion-safe-area-bottom);
+
+  opacity: 1;
+  transition: opacity 0.6s ease-in;
+
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-column-gap: 1em;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "header markers"
+    "debug markers"
+    "footer footer";
+}
+
+.mediaPlayerOverlay.hidden {
+  opacity: 0;
+  transition: opacity 0.6s ease-out;
+}
+
+.mediaPlayerOverlay .playerControls {
+  grid-area: footer;
+}
+
+.mediaPlayerOverlay .playerControls button:first-of-type {
+  margin-left: 0;
+}
+
+.mediaPlayerOverlay .playerControls button {
+  padding: 1em;
+  margin: 0 1em;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: rgb(240, 240, 240);
+}
+@media (prefers-color-scheme: dark) {
+  .mediaPlayerOverlay .playerControls button {
+    background-color: rgba(200, 200, 200, 0.4);
+  }
+}
+
+.mediaPlayerOverlay .overlayHeader {
+  grid-area: header;
+  padding: 0 1em;
+}
+
+.mediaPlayerOverlay .playerDebug {
+  grid-area: debug;
+  display: flex;
+  align-items: end;
+}
+
+.mediaPlayerOverlay .playerDebug .debugInfo {
+  background-color: rgba(100, 100, 100, 0.7);
+  color: rgb(240, 240, 240);
+  padding: 1em;
+  margin-bottom: 3em;
+  border-radius: 0 1.5em 1.5em 0;
+}
+
+h3 button {
+  width: 2em;
+  height: 2em;
+  background-color: rgba(0, 0, 0, 0.4);
+  color: rgb(240, 240, 240);
+  border: 1px solid rgba(230, 230, 230, 0.8);
+}
+@media (prefers-color-scheme: dark) {
+  h3 button {
+    background-color: rgba(200, 200, 200, 0.2);
+  }
+}
+
+.mediaPlayerOverlay .playerDebug .debugInfo button {
+  width: 2em;
+  height: 2em;
+  border: 1px solid rgba(230, 230, 230, 0.8);
+  background-color: rgba(0, 0, 0, 0.4);
+  color: rgb(240, 240, 240);
+}
+
+.mediaPlayerOverlay .markerListContainer {
+  grid-area: markers;
+  min-width: 240px;
+  overflow: scroll;
+}
+
+.mediaPlayerOverlay .markerListContainer ul.markers {
+  padding: 0;
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.mediaPlayerOverlay .markerListContainer ul.markers li {
+  list-style-type: none;
+  padding: 0.8em 0.3em;
+  cursor: pointer;
+  border: 1px solid rgba(255, 255, 255, 0.6);
+}
+
+.mediaPlayerOverlay .markerListContainer ul.markers li.active {
+  background-color: rgba(255, 255, 255, 0.15);
 }
 </style>
